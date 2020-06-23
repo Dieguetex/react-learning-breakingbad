030585c994f98f83f8e392345304930b1e94654a
PEREZ MANSILLA Diego <diego.perez2@soprasteria.com>
2020-06-23 09:14:11 +0200

    Project finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
-import React from "react";
-import styled from "@emotion/style";
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Frase from "./components/Frase";
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 500px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 5rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  cursor: pointer;
+  transition: background-size 0.8s ease;
+
+  &:hover {
+    background-size: 400px;
+  }
+`;
 
 function App() {
-  return <h1>Breaking Bad</h1>;
+  const [frase, guardarFrase] = useState({});
+
+  // Cargar frase
+  useEffect(() => {
+    consultarApi();
+  }, []);
+
+  const consultarApi = async () => {
+    const api = await fetch(
+      "http://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
+  return (
+    <Contenedor>
+      <Frase frase={frase} />
+      <Boton onClick={consultarApi}>Obtener frase</Boton>
+    </Contenedor>
+  );
 }
 
 export default App;
